1b449fafce0874caf4d6b81f46245a0ae72712dd
DEVELOPER\Helenka <olena.kokhan97@gmail.com>
2017-11-17 01:20:19 +0200

    Add test expect

--- chat_test.py
+++ chat_test.py
@@ -48,6 +48,20 @@ class ServerMock:
         self.expect(nick, r":local\S+ 251 %s :.*" % nick)
         self.expect(nick, r":local\S+ 422 %s :.*" % nick)
 
+    def expect(self, nick, regexp):
+        def timeout_handler(signum, frame):
+            raise AssertionError("timeout while waiting for %r" % regexp)
+        signal.signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(1)  # Give the server 1 second to respond
+        line = self.connections[nick].readline().rstrip()
+        signal.alarm(0)  # Cancel the alarm
+        regexp = ("^%s$" % regexp).replace(r"local\S+", socket.getfqdn())
+        m = re.match(regexp, line)
+        if m:
+            return m
+        else:
+            assert_true(False, "Regexp %r didn't match %r" % (regexp, line))
+
 
 def test_some_random_staff():
     server = ServerMock()
